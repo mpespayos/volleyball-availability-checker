bf6ae6bee409e03d7375138f75737867ad6f2bc5
mpespayos <espayos2@gmail.com>
2026-03-08 23:21:18 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const CENTRES = [
+    {
+        name: "Richcraft Kanata (Volleyball - adult)",
+        url: "https://reservation-cf.frontdeskqms.ca/rcfs/richcraftkanata/ReserveTime?culture=en&pageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&buttonId=44882634-f6fd-4241-a313-a192b60f53dd",
+    },
+];
